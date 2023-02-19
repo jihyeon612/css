2bfd6333b21847be139e5e19662e22257b0bf578
jihyeon612 <dhd0612@naver.com>
2023-02-19 23:43:38 +0900

    java array

---
+++ app.js
@@ -0,0 +1,16 @@
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat"];
+
+// Get Item from Array  
+console.log(daysOfWeek[4]);
+
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+
+console.log(toBuy);
